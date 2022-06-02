d0faf17a322c6087d90fa38dd5a9ef322928ce88
Andrew Yeh <ayeh2317@gmail.com>
2022-06-01 23:53:11 -0700

    added jQuery, added functionality to display a question with choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,50 @@
 let quizData;
-function getData(){
-    fetch("./data/quizQuestions.json")  //fetch json
+let correctAnswer = ``;
+let questionDisplayed = ``;
+let quizDataIndex = 0;
+let questionElement = $(`#question`);
+let choicesContainer = $(`#choices`);
+
+function getData() {
+    fetch(`./data/quizQuestions.json`)  //fetch json
     .then(function(response) {  //get response and turn into json
         return response.json();
     })
     .then(function(data) {
-        quizData = data;
+        quizData = data.quiz;
+        displayQuestions();
     });
 }
+
+function displayQuestions() {   //display questions, marks correct one with a certain attribute, randomize choices
+    questionDisplayed = quizData[quizDataIndex].question;   //grab question from current index
+    let choicesArray = quizData[quizDataIndex].choices;     //grab choices for current question
+    correctAnswer = choicesArray[0];                        //grab correct answer
+
+    let choicesIndices = [];                                //create an array from 0 to length-1 of choices
+    for(let i=0; i<choicesArray.length; i++) {
+        choicesIndices.push(i);
+    }
+    choicesIndices = choicesIndices.sort(() => Math.random() - 0.5);    //randomize the choices indexes
+
+    questionElement.text(questionDisplayed);    //set the question displayed on screen
+    choicesContainer.empty();       //clear the choices
+
+    for(let i=0; i<choicesArray.length; i++) {  //for each element in choicesArray, add the choice to the screen using the choicesIndices to make the order random
+        let choiceListItem = $(`<li>`);
+        let choiceButton = $(`<button>`);
+        choiceButton.text(choicesArray[choicesIndices[i]]);
+        choiceListItem.append(choiceButton);
+        choicesContainer.append(choiceListItem);
+    }
+}
+
+choicesContainer.on(`click`, function(event) {
+    let element = event.target;
+    console.log(element.textContent);
+    if(element.textContent === correctAnswer) {
+        console.log('CORRECT');
+    }
+});
 
 getData();
